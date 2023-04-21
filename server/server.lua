5f87cb5a9aac0e5b94a508a1a50aee93142f077d
SirCanario <danifligk@gmail.com>
2023-04-21 16:15:36 +0200

    crno_progressbar
    Process Bar Script!!

---
+++ server/server.lua
@@ -0,0 +1,12 @@
+print("^9---------------------------------------------------^0")
+print("^6Canario Shop | ProgressBar^0")
+print("^6Creador: SirCanario#2866^0")
+print("^6Canario Shop: https://discord.gg/XqWrFymfzP^0")
+print("^6Canario Host: https://canario-hosting.com/^0")
+print("^9---------------------------------------------------^0")
+
+ESX             = nil
+local ShopItems = {}
+local hasSqlRun = false
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
